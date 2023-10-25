a2efccda054279e013d0639bfc8ad975024413ad
21smaloney <75748558+21smaloney@users.noreply.github.com>
2023-10-25 16:46:35 -0400

    Update Yootoob.sql
    Added code for tables.

--- Yootoob.sql
+++ Yootoob.sql
@@ -1,10 +1,53 @@
 -- Let's begin!
 
+-- Drop tables.
+
+DROP TABLE GP_Videos;
+DROP TABLE GP_Influencers;
+DROP TABLE GP_Interactions;
+
+-- Create tables.
+CREATE TABLE GP_Videos (
+    Video_Title VARCHAR2(256) NOT NULL,
+    Duration_Time TIMESTAMP,
+    Duration_Seconds NUMBER,
+    Date_Uploaded DATE,
+    Has_Subtitles VARCHAR2(3),
+    Has_Description VARCHAR2(3),
+    Total_Hashtags NUMBER,
+    Max_Quality NUMBER,
+    
+    PRIMARY KEY (Video_Title)
+);
+    
+CREATE TABLE GP_Influencers (
+    Video_Title VARCHAR2(256) NOT NULL,
+    Creator_Name VARCHAR2(64),
+    Creator_Gender VARCHAR2(6),
+    Total_Subscribers NUMBER,
+    Total_Views NUMBER,
+    Total_Videos NUMBER,
+    Total_Playlists NUMBER,
+    
+    PRIMARY KEY (Video_Title)
+);
+
+CREATE TABLE GP_Interactions (
+    Video_Title VARCHAR2(256) NOT NULL,
+    Total_Likes NUMBER,
+    Total_Comments NUMBER,
+    Last_Comment_Date DATE,
+    Has_Premiered VARCHAR2(3),
+    Weekly_Posts NUMBER,
+    
+    PRIMARY KEY (Video_Title)
+);
+
 -- Question 1 [Alexis]
 
 -- Question 2
 
--- Question 3
+-- Question 3 [Sean]
 
 -- Question 4
 
