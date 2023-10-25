da61f9ff856e9f660681c57b6260a455c4af5d42
Mocha <91145922+mocha-jfif@users.noreply.github.com>
2023-10-25 16:38:58 -0400

    Update Yootoob.sql
    Added question comments

--- Yootoob.sql
+++ Yootoob.sql
@@ -1,1 +1,21 @@
 -- Let's begin!
+
+-- Question 1
+
+-- Question 2
+
+-- Question 3
+
+-- Question 4
+
+-- Question 5
+
+-- Question 6
+
+-- Question 7 
+
+-- Question 8
+
+-- Question 9
+
+-- Question 10
