8f6bfade8b1a867d6d8cbfc95e30ffca381762c3
Mocha <91145922+mocha-jfif@users.noreply.github.com>
2023-10-26 12:26:26 -0400

    Completed Q1

--- Yootoob.sql
+++ Yootoob.sql
@@ -45,6 +45,86 @@ CREATE TABLE GP_Interactions (
 
 -- Question 1 [Alexis]
 
+create or replace function mindur return number is
+    minavgduration number;
+begin
+    select
+        min(avg(v.duration_seconds))
+    into
+        minavgduration
+    from
+        gp_influencers i
+    join
+        gp_videos v on i.video_title = v.video_title
+    group by
+        i.creator_name;
+    
+    return minavgduration;
+end;
+
+create or replace function maxdur return number is
+    maxavgduration number;
+begin
+    select
+        max(avg(v.duration_seconds))
+    into
+        maxavgduration
+    from
+        gp_influencers i
+    join
+        gp_videos v on i.video_title = v.video_title
+    group by
+        i.creator_name;
+    
+    return maxavgduration;
+end;
+
+declare
+    mincreate varchar2(64); 
+    maxcreate varchar2(64); 
+    mindur number;
+    maxdur number;
+    subcountmax number;
+    subcountmin number;
+begin
+    select max(avgduration), max(creator_name)
+    into maxdur, maxcreate
+    from (
+        select avg(v.duration_seconds) as avgduration, c.creator_name
+        from gp_influencers c
+        join gp_videos v on c.video_title = v.video_title
+        group by c.creator_name
+    );
+
+    select min(avgduration), min(creator_name)
+    into mindur, mincreate
+    from (
+        select avg(v.duration_seconds) as avgduration, c.creator_name
+        from gp_influencers c
+        join gp_videos v on c.video_title = v.video_title
+        group by c.creator_name
+    );
+    
+    select total_subscribers
+    into subcountmax
+    from gp_influencers
+    where creator_name = maxcreate;
+    
+    select total_subscribers
+    into subcountmin
+    from gp_influencers
+    where creator_name = mincreate;
+
+    dbms_output.put_line('Influencer with maximum average duration (in seconds): ' || maxcreate);
+    dbms_output.put_line('Average length: ' || maxdur);
+    dbms_output.put_line('Number of subscribers: ' || subcountmax);
+    dbms_output.put_line('- - - - - - - - - - - -');
+    dbms_output.put_line('Influencer with minimum average duration (in seconds): ' || mincreate);
+    dbms_output.put_line('Average length: ' || mindur);
+    dbms_output.put_line('Number of subscribers: ' || subcountmin);
+end;
+
+
 -- Question 2
 
 -- Question 3 [Sean]
